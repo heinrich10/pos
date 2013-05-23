93ac1f4c813f392b4f418dd9290b2262daad70fa
heinrich10 <heinrich10@localhost>
2013-05-24 01:45:14 +0800

    jsch helper class

---
+++ src/main/java/com/app/inv/helper/MyUserInfo.java
@@ -0,0 +1,56 @@
+package com.app.inv.helper;
+
+
+import com.jcraft.jsch.UIKeyboardInteractive;
+import com.jcraft.jsch.UserInfo;
+
+public class MyUserInfo implements UserInfo, UIKeyboardInteractive {
+	
+	
+	@Override
+	public String[] promptKeyboardInteractive(String arg0, String arg1,
+			String arg2, String[] arg3, boolean[] arg4) {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+	@Override
+	public String getPassphrase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+	@Override
+	public boolean promptPassphrase(String arg0) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean promptPassword(String arg0) {
+		// TODO Auto-generated method stub
+		 return false;
+	}
+
+	@Override
+	public boolean promptYesNo(String arg0) {
+		// TODO Auto-generated method stub
+		//return true for the mean time
+		return true;
+	}
+
+	@Override
+	public void showMessage(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
